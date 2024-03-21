0877f33b061f386d6aa769c48f8c82fddd53b50d
night3098 <134074263+night3098@users.noreply.github.com>
2024-03-21 16:25:26 +0700

    Add new items

--- rm_cache.sh
+++ rm_cache.sh
@@ -14,5 +14,21 @@ rm src/CodeKeeper/qrc_qmake_qmake_immediate.o
 rm src/CodeKeeper/qrc_resources.cpp
 rm src/CodeKeeper/qrc_resources.o
 rm src/CodeKeeper/settingswindow.o
+rm src/CodeKeeper/markdownhighlighter.o
+rm src/CodeKeeper/moc_linenumberarea.cpp
+rm src/CodeKeeper/moc_linenumberarea.o
+rm src/CodeKeeper/moc_markdownhighlighter.cpp
+rm src/CodeKeeper/moc_markdownhighlighter.o
+rm src/CodeKeeper/moc_qmarkdowntextedit.cpp
+rm src/CodeKeeper/moc_qmarkdowntextedit.o
+rm src/CodeKeeper/moc_qplaintexteditsearchwidget.cpp
+rm src/CodeKeeper/moc_qplaintexteditsearchwidget.o
+rm src/CodeKeeper/qmarkdowntextedit.o
+rm src/CodeKeeper/qownlanguagedata.o
+rm src/CodeKeeper/qplaintexteditsearchwidget.o
+rm src/CodeKeeper/qrc_media.cpp
+rm src/CodeKeeper/qrc_media.o
+rm src/CodeKeeper/ui_qplaintexteditsearchwidget.h
+
 
 echo "DONE"
