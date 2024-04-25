df1483bdba3929d0b9199e7cbd676c3c10751e3c
Nighty3098 <night3098game@gmail.com>
2024-04-25 16:00:14 +0700

    Sync window

--- src/CodeKeeper/syncwindow.cpp
+++ src/CodeKeeper/syncwindow.cpp
@@ -1,6 +1,39 @@
 #include "syncwindow.h"
 #include <QtWidgets>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
 
-SyncWindow::SyncWindow(QWidget *parent) : QMainWindow(parent) { }
+SyncWindow::SyncWindow(QWidget *parent) : QMainWindow(parent)
+{
+    centralWidget = new QWidget(this);
+    setCentralWidget(centralWidget);
+    this->setWindowFlags(windowFlags() | Qt::FramelessWindowHint);
+
+    globalSettings = new QSettings("CodeKeeper", "CodeKeeper");
+    selectedFont = globalSettings->value("font").value<QFont>();
+    font_size = globalSettings->value("fontSize").value<QString>();
+    dir = globalSettings->value("path").value<QString>();
+
+    mainLayout = new QGridLayout;
+
+    manager = new QNetworkAccessManager();
+    reply = manager->get(QNetworkRequest(QUrl("http://www.google.com")));
+
+    mainTitle = new QLabel("");
+
+    mainLayout->addWidget(mainTitle, 3, 0, 1, 1);
+
+    centralWidget->setLayout(mainLayout);
+
+    QObject::connect(reply, &QNetworkReply::finished, [=]() {
+        if (reply->error() == QNetworkReply::NoError) {
+            qDebug() << "Internet connection is available";
+        } else {
+            qDebug() << "No internet connection found";
+        }
+    });
+}
 
 SyncWindow::~SyncWindow() { }
+
+void SyncWindow::checkConnection() { }
--- src/CodeKeeper/syncwindow.h
+++ src/CodeKeeper/syncwindow.h
@@ -2,19 +2,40 @@
 #define SYNCWINDOW_H
 
 #include <QtWidgets>
+#include <QSettings>
+#include <QGridLayout>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+
 
 class SyncWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
+    QSettings *globalSettings;
+    
     SyncWindow(QWidget *parent = nullptr);
     ~SyncWindow();
 
+    QFont selectedFont;
+    QString font_size;
+    QString dir;
+
 private slots:
+    void checkConnection();
 
 protected:
 private:
+    QWidget *centralWidget;
+    QGridLayout *mainLayout;
+
+    QLabel *mainTitle;
+    QLabel *checkConnectionL;
+
+    QNetworkAccessManager* manager;
+    QNetworkReply* reply;
+
 };
 
 #endif // MAINWINDOW_H
