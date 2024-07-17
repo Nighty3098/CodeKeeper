9637b030c1ff6370899e560c383e276101a4a397
Nighty3098 <154594695+Nighty3098@users.noreply.github.com>
2024-07-17 23:26:48 +0700

    Added hover effect for QTabWidget button

--- src/CodeKeeper/keeperFunc/functional.cpp
+++ src/CodeKeeper/keeperFunc/functional.cpp
@@ -535,10 +535,18 @@ void MainWindow::setStyle(QFont* selectedFont, int* font_size_int)
     openAccountWindow->setFont(*selectedFont);
     openAccountWindow->setStyleSheet("QPushButton {border: none; background-color: transparent; font-size: " + font_size + "pt;} ");
 
-    mainTabButton->setStyleSheet("background-color: transparent; border: none;");
-    tasksTabButton->setStyleSheet("background-color: transparent; border: none;");
-    notesTabButton->setStyleSheet("background-color: transparent; border: none;");
-    projectsTabButton->setStyleSheet("background-color: transparent; border: none;");
+    mainTabButton->setStyleSheet("QPushButton {background-color: transparent; border: none;} QPushButton:pressed {background-color: transparent; "
+                                 "border: none;} QPushButton:focus{border: none; background-color: transparent; border-right: 5px solid #36bcf7;} "
+                                 "QPushButton:hover{border: none; background-color: transparent; border-right: 5px solid #36bcf7;}");
+    tasksTabButton->setStyleSheet("QPushButton {background-color: transparent; border: none;} QPushButton:pressed {background-color: transparent; "
+                                  "border: none;} QPushButton:focus{border: none; background-color: transparent; border-right: 5px solid #36bcf7;} "
+                                  "QPushButton:hover{border: none; background-color: transparent; border-right: 5px solid #36bcf7;}");
+    notesTabButton->setStyleSheet("QPushButton {background-color: transparent; border: none;} QPushButton:pressed {background-color: transparent; "
+                                  "border: none;} QPushButton:focus{border: none; background-color: transparent; border-right: 5px solid #36bcf7;} "
+                                  "QPushButton:hover{border: none; background-color: transparent; border-right: 5px solid #36bcf7;}");
+    projectsTabButton->setStyleSheet("QPushButton {background-color: transparent; border: none;} QPushButton:pressed {background-color: transparent; "
+                                     "border: none;} QPushButton:focus{border: none; background-color: transparent; border-right: 5px solid "
+                                     "#36bcf7;} QPushButton:hover{border: none; background-color: transparent; border-right: 5px solid #36bcf7;}");
 
     setH1B->setStyleSheet("background-color: transparent; border: none; margin-left: 4px;");
     setH2B->setStyleSheet("background-color: transparent; border: none; margin-left: 4px;");
@@ -575,9 +583,11 @@ void MainWindow::setStyle(QFont* selectedFont, int* font_size_int)
     setQuoteB->setToolTip("<p style='color: #ffffff; border-radius: 5px; background-color: "
                           "#0D1117;'>Set quote</p>");
 
-    menuButton->setStyleSheet("background-color: transparent; border: none;");
+    menuButton->setStyleSheet("border: 5px; border-radius: 10px;");
 
-    tasksMenuBtn->setStyleSheet("background-color: transparent; border: none; border-width: 0px;");
+    tasksMenuBtn->setStyleSheet("border: 5px; border-radius: 10px;");
+
+    projectsMenuButton->setStyleSheet("border: 5px; border-radius: 10px;");
 
     isAutoSync->setStyleSheet("border: 0px; background-color: transparent;");
 
@@ -590,8 +600,6 @@ void MainWindow::setStyle(QFont* selectedFont, int* font_size_int)
     sProjects->setStyleSheet("font-size: " + font_size + "px;");
 
     nsProjects->setStyleSheet("font-size: " + font_size + "px;");
-
-    projectsMenuButton->setStyleSheet("background-color: transparent; border: none; border-width: 0px;");
 
     label_3->setStyleSheet("font-size: " + font_size + "px;");
 
