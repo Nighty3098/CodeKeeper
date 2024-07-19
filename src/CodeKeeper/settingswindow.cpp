5d693b81764550dbb9fc85678f87a631cb0ac1ca
Nighty3098 <154594695+Nighty3098@users.noreply.github.com>
2024-07-19 18:32:33 +0700

    New version

--- src/CodeKeeper/settingswindow.cpp
+++ src/CodeKeeper/settingswindow.cpp
@@ -89,7 +89,7 @@ SettingsWindow::SettingsWindow(QWidget* parent) : QMainWindow { parent }
     urlToRepo->setAlignment(Qt::AlignHCenter | Qt::AlignTop);
 
     versionInfo = new QLabel();
-    versionInfo->setText("0.1.8");
+    versionInfo->setText("0.1.9");
     versionInfo->setAlignment(Qt::AlignCenter);
 
     checkUpdatesBtn = new QPushButton(
