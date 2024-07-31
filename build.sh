c8c07c407655bf37ffbd8867b73f6aa1bfc5885c
Nighty3098 <154594695+Nighty3098@users.noreply.github.com>
2024-07-31 20:49:27 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-cd src/CodeKeeper
+cd src/CodeKeeper || { echo “Failed to go to src/CodeKeeper directory”; exit 1; }
+
 qmake CodeKeeper.pro
-make
-cd ../..
+
+echo Start compilation...
+{ time make; } 2> compilation_time.txt
+
+echo “Compilation complete.”
+echo “Compilation time:”
+cat compilation_time.txt
+rm compilation_time.txt
+
+cd ../.. || { echo “Failed to go to root directory”; exit 1; }
